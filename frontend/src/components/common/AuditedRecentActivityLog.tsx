18afcc08f53885d953eab512b8cf9cb0f99eabb1
dileepreddy-skilgobal <dileepreddy@skilglobal.com>
2026-09-08 16:35:49 +0530

    chore: clean up obsolete GCP scripts, unused mock APIs, dead components, and temporary files

--- frontend/src/components/common/AuditedRecentActivityLog.tsx
+++
@@ -1,9 +0,0 @@
-import { RecentActivityLog } from "../ui/PremiumUI";
-
-interface AuditedRecentActivityLogProps {
-  entityName?: string;
-}
-
-export function AuditedRecentActivityLog({ entityName }: AuditedRecentActivityLogProps) {
-  return <RecentActivityLog entityType={entityName} />;
-}
